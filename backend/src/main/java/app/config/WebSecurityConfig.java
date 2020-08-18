0c8add90f03a0e55a7556641892952fa660cdcea
danilo.meneghel <danilo.meneghel@gmail.com>
2020-08-18 15:25:43 -0300

    Adicionado novamente Resources no pom para poder carregar os templates com Thymeleaf de forma correta

--- backend/src/main/java/app/config/WebSecurityConfig.java
+++ backend/src/main/java/app/config/WebSecurityConfig.java
@@ -23,8 +23,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-            .authorizeRequests().antMatchers("/login", "/favicon.ico", "/**/*.html", "/**/*.css", "/**/*.js", 
-            "/images/**", "/fonts/**", "/swagger**", "/api/**", "/cadastro/**", "/salvarCadastro").permitAll() 
+            .authorizeRequests().antMatchers("/login", "/favicon.ico", "/**/*.html", "/**/*.css", "/**/*.js", "/**/*.eot", "/**/*.ttf", 
+            "/**/*.woff", "/**/*.woff2", "/images/**", "/fonts/**", "/swagger**", "/api/**", "/cadastro/**", "/salvarCadastro").permitAll() 
             .and()
             .csrf().disable()
             .authorizeRequests()
